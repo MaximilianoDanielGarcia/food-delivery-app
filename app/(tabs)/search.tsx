fb9eb55156e21df30ff1d232b2d51b689ff73efe
Maximiliano Daniel García <maxi.garcia11@hotmail.com>
2025-07-19 10:44:16 -0300

    Implement search & filter

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,75 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import CardButton from "@/components/CardButton";
+import { getCategories, getMenu } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect } from "react";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import cn from 'clsx'
+import MenuCard from "@/components/MenuCard";
+import { Category, MenuItem } from "@/type";
+import SearchBar from "@/components/SearchBar";
+import Filter from "@/components/Filter";
 
 const Search = () => {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+  const { category, query } = useLocalSearchParams<{
+    query: string;
+    category: string;
+  }>();
 
-export default Search
+  const { data, refetch, loading } = useAppwrite({
+    fn: getMenu,
+    params: {
+      category,
+      query,
+      limit: 6,
+    },
+  });
+
+  useEffect(() => {
+    refetch({ category, query, limit: 6 });
+  }, [category, query]);
+
+  const { data: categories } = useAppwrite({ fn: getCategories });
+
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => {
+            const isFirstRightColItem = index % 2 === 0;
+          return (
+            <View className={cn("flex-1 max-w-[48%]", !isFirstRightColItem ? 'mt-10': 'mt-0')}>
+              <MenuCard item={item as MenuItem}/>
+            </View>
+          );
+        }}
+        keyExtractor={item => item.$id}
+        numColumns={2}
+        columnWrapperClassName="gap-7"
+        contentContainerClassName="gap-7 px-5 pb-32"
+        ListHeaderComponent={() => (
+            <View className="my-5 gap-5">
+                <View className="flex-between flex-row w-full">
+                    <View className="flex-start">
+                        <Text className="small-bold uppercase text-primary">Search</Text>
+                        <View className="flex-start flex-row gap-x-1 mt-0.5">
+                            <Text className="paragraph-semibold text-dark-100">Find your favorite food</Text>
+                        </View>
+                    </View>
+
+                    <CardButton />
+                </View>
+
+                <SearchBar />
+
+                <Filter categories={categories as Category[]} />
+            </View>
+        )}
+        ListEmptyComponent={() => !loading && <Text>No results.</Text>}
+      ></FlatList>
+    </SafeAreaView>
+  );
+};
+
+export default Search;
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { CreateUserParams, SignInParams } from "@/type";
+import { CreateUserParams, GetMenuParams, SignInParams } from "@/type";
 import {
   Account,
   Avatars,
@@ -6,26 +6,37 @@ import {
   Databases,
   ID,
   Query,
+  Storage,
 } from "react-native-appwrite";
 
-export const appwrtieConfig = {
+export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
   userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID,
+  categoriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_CATEGORIES_COLLECTION_ID,
+  menuCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_COLLECTION_ID,
+  customizationCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_CUSTOMIZATION_COLLECTION_ID,
+  menuCustomizationCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_MENU_CUSTOMIZATIONS_COLLECTION_ID,
+  bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
 };
 
 export const client = new Client();
 
 client
-  .setEndpoint(appwrtieConfig.endpoint!)
-  .setProject(appwrtieConfig.projectId!)
-  .setPlatform(appwrtieConfig.platform!);
+  .setEndpoint(appwriteConfig.endpoint!)
+  .setProject(appwriteConfig.projectId!)
+  .setPlatform(appwriteConfig.platform!);
 
 export const account = new Account(client);
 
 export const databases = new Databases(client);
+
+export const storage = new Storage(client);
 
 const avatars = new Avatars(client);
 
@@ -44,8 +55,8 @@ export const createUser = async ({
     const avatarUrl = avatars.getInitialsURL(name);
 
     return await databases.createDocument(
-      appwrtieConfig.databaseId!,
-      appwrtieConfig.userCollectionId!,
+      appwriteConfig.databaseId!,
+      appwriteConfig.userCollectionId!,
       ID.unique(),
       {
         accountId: newAccount.$id,
@@ -73,17 +84,48 @@ export const getCurrentUser = async () => {
     if (!currentAccount) throw Error;
 
     const currentUser = await databases.listDocuments(
-      appwrtieConfig.databaseId!,
-      appwrtieConfig.userCollectionId!,
-      [Query.equal('accountId', currentAccount.$id)]
+      appwriteConfig.databaseId!,
+      appwriteConfig.userCollectionId!,
+      [Query.equal("accountId", currentAccount.$id)]
     );
 
-    if(!currentUser) throw Error;
+    if (!currentUser) throw Error;
 
     return currentUser.documents[0];
-
   } catch (e) {
     console.log(e);
+    throw new Error(e as string);
+  }
+};
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+  try {
+    const queries: string[] = [];
+
+    if (category) queries.push(Query.equal("categories", category));
+    if (query) queries.push(Query.search("name", query));
+
+    const menus = await databases.listDocuments(
+      appwriteConfig.databaseId!,
+      appwriteConfig.menuCollectionId!,
+      queries
+    );
+
+    return menus.documents;
+  } catch (e) {
+    throw new Error(e as string);
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const categories = await databases.listDocuments(
+      appwriteConfig.databaseId!,
+      appwriteConfig.categoriesCollectionId!
+    );
+
+    return categories.documents;
+  } catch (e) {
     throw new Error(e as string);
   }
 };
